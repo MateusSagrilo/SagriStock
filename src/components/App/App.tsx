044b41f690e19440f6480b95917106bdc42a4e54
Mateus Sagrilo <sagrilomateus@gmail.com>
2025-12-22 20:29:01 -0300

    clear whole application

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-// import ClassComponent from '../ClassComponent';
-import FuncComponent from '../FuncComponent';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <ClassComponent name="Mundo"/> */}
-        <FuncComponent name='Mateus'/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App
     </div>
   );
 }
