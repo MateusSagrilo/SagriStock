7231e54168775578156b4bc508373bd67d5a6efa
Mateus Sagrilo <sagrilomateus@gmail.com>
2025-12-22 20:17:16 -0300

    disable class component from app and add useEffect hook

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ClassComponent from '../ClassComponent';
+// import ClassComponent from '../ClassComponent';
 import FuncComponent from '../FuncComponent';
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <ClassComponent name="Mundo"/>
+        {/* <ClassComponent name="Mundo"/> */}
         <FuncComponent name='Mateus'/>
         <a
           className="App-link"
--- src/components/FuncComponent/FuncComponent.tsx
+++ src/components/FuncComponent/FuncComponent.tsx
@@ -1,8 +1,18 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './FuncComponent.css'
 
 function FuncComponent (props: {name: string}) {
     const [age, setAge] = useState(30)
+
+    useEffect(() => {
+        console.log('component was created');
+        
+    }, [])
+
+    useEffect(() => {
+        console.log('Age has been updated to: ' + age);
+        
+    }, [age])
 
     return <div className='FuncComponent'>
         Olá, {props.name}, {age}
