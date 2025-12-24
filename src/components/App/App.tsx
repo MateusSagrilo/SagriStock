1e4afeb0e961e88ae89c7d1b0d6d57f13666739c
Mateus Sagrilo <sagrilomateus@gmail.com>
2025-12-23 21:13:43 -0300

    add button component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header';
+import Button from '../Button';
 
 
 function App() {
   return (
     <div className="App">
       <Header title='AlgaStock'/>
-      App
+      <div className='Container'>
+        <Button content="Click me"/>
+      </div>
     </div>
   );
 }
