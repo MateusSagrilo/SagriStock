f99b4539005df6136928bc08812d9bb372a88943
Mateus Sagrilo <sagrilomateus@gmail.com>
2025-12-24 20:35:06 -0300

    add input component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from '../Header';
 import Button from '../../shared/Button';
 import Container from '../../shared/Container/Container';
+import Input from '../../shared/Input';
 
 
 function App() {
+
+  const [street, setStreet] =useState('')
   return (
     <div className="App">
       <Header title='AlgaStock'/>
@@ -15,6 +18,12 @@ function App() {
           >
             Alert
         </Button>
+        <Input
+          label='Street'
+          placeholder='E.g.: 15h Avenue'
+          value={street}
+          onChange= {e => setStreet(e.target.value)}
+          />
       </Container>
         
       </div>
