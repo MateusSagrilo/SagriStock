9eaaca515be2a96a7c5cd81c684e6a5ca577daf1
Mateus Sagrilo <sagrilomateus@gmail.com>
2025-12-26 22:05:54 -0300

    table mockdata

--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import './Table.scss'
+import Products from './Table.mockdata'
 
+const headers = [
+  {key: 'name', value: 'Product'},
+  {key: 'price', value: 'Price'},
+  {key: 'stock', value: 'Available Stock'}
+]
 const Table = () => {
   return <table className='AppTable'>
     <thead>
       <tr>
-        <th>Product</th>
-        <th>Price</th>
-        <th className='right'>Stock</th>
+        {
+          headers.map(header => <th key={header.key}>{header.value}</th>)
+        }
       </tr>
     </thead>
     <tbody>
