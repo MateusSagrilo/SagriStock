eb985a817889b512212862b588029802ad126a08
Mateus Sagrilo <sagrilomateus@gmail.com>
2025-12-29 20:41:49 -0300

    add product-form component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,50 +1,26 @@
-import React from 'react';
-import './App.css';
-import Header from '../Header';
-import Container from '../../shared/Container';
-import Table, {TableHeader} from '../../shared/Table';
-import Products from '../../shared/Table/Table.mockdata';
-import Form from '../../shared/Form';
-import Input from '../../shared/Input';
-import Button from '../../shared/Button';
-
+import React from "react";
+import "./App.css";
+import Header from "../Header";
+import Container from "../../shared/Container";
+import Table, { TableHeader } from "../../shared/Table";
+import Products from "../../shared/Table/Table.mockdata";
+import ProductForm from "../Products/ProductForm";
 
 const headers: TableHeader[] = [
-  { key: 'id', value: '#' },
-  { key: 'name', value: 'Product' },
-  { key: 'price', value: 'Price', right: true },
-  { key: 'stock', value: 'Available Stock', right: true }
-]
+  { key: "id", value: "#" },
+  { key: "name", value: "Product" },
+  { key: "price", value: "Price", right: true },
+  { key: "stock", value: "Available Stock", right: true },
+];
 
 function App() {
   return (
-   <div className="App">
+    <div className="App">
       <Header title="SagriStock" />
       <Container>
-        <Table
-          headers={headers}
-          data={Products}
-        />
+        <Table headers={headers} data={Products} />
 
-        <Form title='Product Form' >
-          <Input
-            label='Name'
-            placeholder='E.g.: Cookie'/>
-          <Input
-            label='Price'
-            type='number'
-            step='0.01'
-            min='0'
-            placeholder='E.g.: $2.00'/>
-          <Input
-            label='Stock'
-            type='number'
-            min='0'
-            placeholder='E.g.: 10'/>
-        </Form>
-        <Button>
-          Submite
-        </Button>
+        <ProductForm/>
       </Container>
     </div>
   );
