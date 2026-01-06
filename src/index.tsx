49617d24dc2dc36567ba4518c718240c328cd9c4
Mateus Sagrilo <sagrilomateus@gmail.com>
2026-01-05 21:26:26 -0300

    add axios to the project

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import http from './utils/http';
 
+http.get('/posts')
+http.get('/posts/1')
+http.get('https://viacep.com.br/ws/29140160/json/')
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-
