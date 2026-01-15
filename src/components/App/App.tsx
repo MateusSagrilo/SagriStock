ac44a52df85a6428be78110c826d509abbfc8736
Mateus Sagrilo <sagrilomateus@gmail.com>
2026-01-15 19:55:58 -0300

    add home view + react-router-dom

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,21 +1,17 @@
-import React from "react";
-import "./App.css";
-import Header from "../Header";
-import Container from "../../shared/Container";
-import ProductsCRUD from "../Products/ProductsCRUD";
-
-
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css';
+import HomeView from '../../views/HomeView';
 
 
 function App() {
- 
-
   return (
     <div className="App">
-      <Header title="SagriStock" />
-      <Container>
-        <ProductsCRUD/>
-      </Container>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomeView />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
