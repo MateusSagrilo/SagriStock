b7fee2df4bd1bbc3ed36ff583e06911ec7452784
Mateus Sagrilo <sagrilomateus@gmail.com>
2026-01-17 17:30:34 -0300

    created profile view + card

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import HomeView from '../../views/HomeView';
 import NotFoundView from '../../views/NotFoundView';
 import LoginView from '../../views/LoginView';
+import ProfileView from '../../views/ProfileView';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomeView />} />
           <Route path="/login" element={<LoginView />} />
+          <Route path="/profile" element={<ProfileView />} />
           <Route path="*" element={<NotFoundView />} />
         </Routes>
       </BrowserRouter>
