db7931a85944f69ae8badddf93c1bf5a573d0303
Mateus Sagrilo <sagrilomateus@gmail.com>
2026-02-16 20:01:41 -0300

    remove source map and fix withPermession type

--- src/utils/HOC/withPermission.tsx
+++ src/utils/HOC/withPermission.tsx
@@ -1,26 +1,27 @@
-import React, { FC, ReactNode } from 'react'
-import { useSelector } from 'react-redux'
-import { RootState } from '../../redux'
-import { useNavigate } from 'react-router-dom'
+import React, { FC } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux";
+import { useNavigate } from "react-router-dom";
 
-type Role = 'admin' | 'customer' | undefined
+type Role = "admin" | "customer" | undefined;
 
-const withPermission = (roles: Role[], redirect = '') => 
-  (Component: FC<any>): FC<any> => 
-  (props: any): ReactNode => {
-    const navigate = useNavigate()
+const withPermission =
+  (roles: Role[], redirect = "") =>
+  (Component: FC<any>): FC<any> =>
+  (props: any): React.ReactElement<any, any> | null => {
+    const navigate = useNavigate();
     const auth = useSelector((state: RootState) => ({
-      profile: state.authentication.profile
-    }))
+      profile: state.authentication.profile,
+    }));
 
     if (!roles.includes(auth.profile?.role)) {
       if (redirect) {
-        navigate(redirect)
+        navigate(redirect);
       }
-      return null
+      return null;
     }
 
-    return <Component {...props} />
-  }
+    return <Component {...props} />;
+  };
 
-export default withPermission
+export default withPermission;
